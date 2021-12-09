6d595defa467b602c761067639301af2753a8624
Nikosh Maglinao <maglinaonikosh@gmail.com>
2021-12-09 11:09:23 +0800

    commit

--- cpfl_PT4.cpp
+++ cpfl_PT4.cpp
@@ -8,17 +8,24 @@
 #include<iomanip>
 #include<cstring>
 #include<string>
+#include<cstdlib>
 
 using namespace std;
 
 int main(){
+
+    ifstream in_stream;
+    ofstream ou_stream;
+
     char username[10];
     char password[10];
     string name;
-    int eng, mth, scs, prgm, pe; //eng-english, mth-math, scs-science, prgm-programming, pe-physical education
+    int eng, mth, scs, prgm, pe; 
     int sum;
     long long int average;
-    
+
+    in_stream.open("in_file.txt");
+    ou_stream.open("ou_file.txt", ios::app);
 
     cout << "\n Enter your Login Credentials " <<endl;
     cout << "Enter your Username: "; 
@@ -50,6 +57,18 @@ int main(){
     sum = eng +mth +scs +prgm +pe;
     average = sum / 5;
 
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
+    ou_stream <<"Enter your Name : "<<endl;
+    ou_stream <<"\nGrade in English       : "<< eng <<endl;
+    ou_stream <<"Grade in Mathematics     : "<< mth <<endl;
+    ou_stream <<"Grade in Science         : "<< scs <<endl;
+    ou_stream <<"Grade in C++ Programming : "<< prgm <<endl;
+    ou_stream <<"Grade in Phys. Education : "<< pe << endl;
+    ou_stream <<"\nYour Average is : "<< average <<endl;
+    ou_stream <<endl;
     cout<<"\nYour Average is : "<<average <<endl;
 
     if (average >= 95 && average <=100)
@@ -86,6 +105,8 @@ int main(){
     { 
          cout << ("\nThe user you've input does not exist!") <<endl;
     } 
-  
+    
+    in_stream.close();
+    ou_stream.close();
     return 0; 
 }
